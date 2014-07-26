cdffdf5df846766b7c3986e48f78a5e5fc6304f2
Sascha Wise <wisesascha@gmail.com>
2014-07-26 09:17:06 -0700

    Added users key

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -2,4 +2,5 @@ class TagSerializer < ActiveModel::Serializer
     attributes :id,:name,:description
     has_many :videos, embed: :id, :key => :video_ids
     has_many :subscribers, embed: :id, :key => :subscriber_ids
+    has_many :users , embed: :id, :key => :user_ids
 end
