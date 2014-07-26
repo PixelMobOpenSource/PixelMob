068abc98c5161f8e7ec83fd78e38708680da413a
Sascha Wise <wisesascha@gmail.com>
2014-07-26 09:04:53 -0700

    Reordered methods

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,40 +1,43 @@
 class Api::CommentsController < ApplicationController
-    def show
-        render :json => Comment.find(params[:id])
+  def show
+    render json: Comment.find(params[:id])
+  end
+
+  def index
+    render json: Comment.all
+  end
+
+  def create
+    comment = Comment.new
+    comment.parent_comment_id = params[:comment][:parent_comment_id]
+    if params[:comment][:child_comment_ids]
+      comment.child_comment_ids = params[:comment][:child_comment_ids]
     end
-    def index
-        render :json => Comment.all
+    comment.user_id = params[:comment][:user_id]
+    comment.video_id = params[:comment][:video_id]
+    comment.content = Sanitize.clean(params[:comment][:content], Sanitize::Config::BASIC)
+    if comment.save! && user_signed_in? && current_user == comment.user
+      render json: comment
+    else
+      render json: comment.errors
     end
-    def create
-        comment = Comment.new()
-        comment.parent_comment_id = params[:comment][:parent_comment_id]
-        if(params[:comment][:child_comment_ids]) then
-            comment.child_comment_ids = params[:comment][:child_comment_ids]
-        end
-        comment.content = params[:comment][:content]
-        comment.user_id = params[:comment][:user_id]
-        comment.video_id = params[:comment][:video_id]
-        html = params[:comment][:content]
-        comment.content = Sanitize.clean(html, Sanitize::Config::BASIC)
-        if comment.save! && user_signed_in? && current_user == comment.user
-            render :json => comment
-        else
-            render :json => comment.errors
-        end
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    comment.update_attributes(comment_params)
+    if comment.save! && user_signed_in? && current_user == comment.user
+      render json: comment
+    else
+      render json: comment.errors
     end
-    def update
-        comment = Comment.find(params[:id])
-        comment.update_attributes(comment_params)
-        if comment.save! && user_signed_in? && current_user == comment.user
-            render :json => comment
-        else
-            render :json => comment.errors
-        end
-    end
+  end
+
 private
-    def comment_params
-        if(params[:comment]) then
-            params.require(:comment).permit!
-        end
+
+  def comment_params
+    if params[:comment]
+      params.require(:comment).permit!
     end
+  end
 end
--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,136 +1,147 @@
 class Api::UserController < ApplicationController
-    before_filter :set_current_user
-    def index
-        render :json => User.all
-    end
-    def logout_clef
-        data = {
-            body: {
-                logout_token: params[:logout_token],
-                app_id: '5da008568d5b331c24cf0820cb32fd74',
-                app_secret: 'a5d3313e240da8b306db317a1a7a47cb'
-            }
+  before_filter :set_current_user
+  def index
+    render json: User.all
+  end
+  def logout_clef
+    data = {
+      body: {
+        logout_token: params[:logout_token],
+        app_id: '5da008568d5b331c24cf0820cb32fd74',
+        app_secret: 'a5d3313e240da8b306db317a1a7a47cb'
+      }
+    }
+    response = HTTParty.post("https://clef.io/api/v1/logout",data)
+    if(response["success"])
+      clef_id = response["clef_id"]
+      if(user = User.where(clef_id: clef_id).first) then
+        logger.debug(clef_id)
+        logger.debug(user.email)
+        EM.run{
+        client = Faye::Client.new("http://localhost:8008/faye")
+        client.add_extension(ClientAuth.new)
+        publication = client.publish('/userSessionInfo/' + user.id, :action => "sign_out")
         }
-        response = HTTParty.post("https://clef.io/api/v1/logout",data)
-        if(response["success"])
-            clef_id = response["clef_id"]
-            user = User.find_by(clef_id: clef_id)
-            logger.debug(clef_id)
-            sign_out(user)
-            render :json => {success: "Success"}
-        else
-            render :json => response["error"]
+    end
+    else
+      render json: response["error"]
+    end
+  end
+  def clef
+    p request.env['omniauth.auth']
+    @user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      notice = "User was created."
+    end
+  end
+  def launchkey_callback
+    if(params[:deorbit]) then
+      user = User.find_by(user_key: JSON.parse(params[:deorbit])[:user_hash])
+      logger.debug(user.email)
+      sign_out(user)
+    end
+  end
+  def launchkey_newuser
+    auth_request = LaunchKey.authorize(params[:username])
+    until (LaunchKey.poll_request(auth_request))
+        sleep(1)
+    end
+    auth_response = LaunchKey.poll_request(auth_request)
+    if LaunchKey.authorized?(auth_response["auth"]) then
+      if(!params[:user_id]) then
+        user = User.new
+        user.email = params[:email]
+        user.user_key = auth_response["user_hash"]
+        user.auth_req = auth_request
+        user.save!
+      else
+        render text: 'false'
+      end
+    else
+      render text: "false"
+    end
+  end
+  def launchkey
+    auth_request = LaunchKey.authorize(params[:username])
+    until (LaunchKey.poll_request(auth_request))
+      sleep(1)
+    end
+    auth_response = LaunchKey.poll_request(auth_request)
+    if LaunchKey.authorized?(auth_response["auth"]) then
+      if(params[:user_id]) then
+        user = User.find(params[:user_id])
+        user.user_key = auth_response["user_hash"]
+        user.auth_req = auth_request
+        user.save!
+      else
+        sign_in(User.find_by(user_key: auth_response["user_hash"]))
+        render text: "true"
+      end
+    else
+      render text: "false"
+    end
+  end
+  def show
+    render json: User.find(params[:id])
+  end
+  def subscriptions
+    render json: User.subscriptions_channels
+  end
+  def unsubscribe
+    channel = Channel.find(params[:id])
+    current_user.subscriptions_channels.delete(channel)
+    if current_user.save! then
+      render json: "Success"
+    else
+      render json: current_user.errors
+    end
+  end
+  def subscribe
+    channel = Channel.find(params[:id])
+    current_user.subscriptions_channels << channel
+    if current_user.save! then
+      render json: "Success"
+    else
+      render json: current_user.errors
+    end
+  end
+  def subscriptions_videos
+      videos = []
+      current_user.subscriptions_channels.each do |u|
+        u.videos.each do |v|
+          videos << v
         end
+      end
+      videos_sorted = videos.sort_by{|v| v.created_at}.reverse
+      render json:  videos_sorted
+  end
+  def current
+    render json: current_user
+  end
+  def create
+    clean_params = user_params
+    clean_params.delete(:subscriptions_video_ids)
+    clean_params.delete(:subscription_channel_ids)
+    user = User.new(clean_params)
+    if user.save! then
+      render json: user
+    else
+      render json: user.errors
     end
-    def clef
-        p request.env['omniauth.auth']
-        @user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
-        if @user.persisted?
-            sign_in_and_redirect @user, :event => :authentication
-        else
-            notice = "User was created."
-        end
+  end
+  def update
+    user = User.find(params[:id])
+    if(user.update_attributes(user_params) && user == current_user) then
+      render json: user
     end
-    def launchkey_callback
-        if(params[:deorbit]) then
-            user = User.find_by(user_key: JSON.parse(params[:deorbit])[:user_hash])
-            logger.debug(user.email)
-            sign_out(user)
-        end
-    end
-    def launchkey_newuser
-        auth_request = LaunchKey.authorize(params[:username])
-        until (LaunchKey.poll_request(auth_request)["message"] != "Pending response")
-                sleep(1)
-        end
-        auth_response = LaunchKey.poll_request(auth_request)
-        if LaunchKey.authorized?(auth_response["auth"]) then
-            if(!params[:user_id]) then
-                user = User.new
-                user.email = params[:email]
-                user.user_key = auth_response["user_hash"]
-                user.auth_req = auth_request
-                user.save!
-            else
-                render :text => 'false'
-            end
-        else
-            render :text => "false"
-        end
-    end
-    def launchkey
-        auth_request = LaunchKey.authorize(params[:username])
-        until (LaunchKey.poll_request(auth_request)["message"] != "Pending response")
-            sleep(1)
-        end
-        auth_response = LaunchKey.poll_request(auth_request)
-        if LaunchKey.authorized?(auth_response["auth"]) then
-            if(params[:user_id]) then
-                user = User.find(params[:user_id])
-                user.user_key = auth_response["user_hash"]
-                user.auth_req = auth_request
-                user.save!
-            else
-                sign_in(User.find_by(user_key: auth_response["user_hash"]))
-                render :text => "true"
-            end
-        else
-            render :text => "false"
-        end
-    end
-    def show
-        render :json => User.find(params[:id])
-    end
-    def subscriptions
-        render :json => User.subscriptions_channels
-    end
-    def unsubscribe
-        channel = Channel.find(params[:id])
-        current_user.subscriptions_channels.delete(channel)
-        if current_user.save! then
-            render :json => "Success"
-        else
-            render :json => current_user.errors
-        end
-    end
-    def subscribe
-        channel = Channel.find(params[:id])
-        current_user.subscriptions_channels << channel
-        if current_user.save! then
-            render :json => "Success"
-        else
-            render :json => current_user.errors
-        end
-    end
-    def subscriptions_videos
-            videos = []
-            current_user.subscriptions_channels.each do |u|
-                u.videos.each do |v|
-                    videos << v
-                end
-            end
-            videos_sorted = videos.sort_by{|v| v.created_at}.reverse
-            render :json =>    videos_sorted
-    end
-    def current
-        render :json => current_user
-    end
-    def create
-        user = User.new(user_params)
-        if user.save! then
-            render :json => user
-        else
-            render :json => user.errors
-        end
-    end
-    def update
-        user = User.find(params[:id])
-        if(user.update_attributes(user_params) && user == current_user) then
-            render :json => user
-        end
-    end
-    private
-    def user_params
-        params.require(:user).permit!
-    end
+  end
+  private
+  def user_params
+    new_params = params
+    new_params[:user].delete(:subscriptions_video_ids)
+    new_params[:user].delete(:subscription_channel_ids)
+    new_params.require(:user).permit!
+  end
 end
